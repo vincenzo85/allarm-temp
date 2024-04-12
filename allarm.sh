329669dbdcd057aff6b74f4559e0a5c74956f5e3
vincenzo di franco <vincenzo.difranco@gmail.com>
2024-04-12 13:19:12 +0200

    Update allarm.sh
    change dir

--- allarm.sh
+++ allarm.sh
@@ -7,7 +7,7 @@ while true; do
     # Check if any temperature exceeds 50 degrees
     if [ "$(echo "$temp > 86" | bc -l)" -eq 1 ]; then
       # Play an alarm sound; ensure the path to your actual audio file is correct
-         cvlc --play-and-exit /home/vincenzo/Scrivania/allarm.mp3
+         cvlc --play-and-exit ./allarm.mp3
 
     fi
   done
